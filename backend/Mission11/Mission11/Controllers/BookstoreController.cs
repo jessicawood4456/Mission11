fd07d4a77b8d867f3a34df15ab3f5085737b739c
Jessica Lasson <jessicawood4456@gmail.com>
2025-03-20 16:11:52 -0600

    Initial commit

---
+++ backend/Mission11/Mission11/Controllers/BookstoreController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mission11.Data;
+
+namespace Mission11.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BookstoreController : ControllerBase
+    {
+        private BookstoreDbContext _context;
+        
+        public BookstoreController(BookstoreDbContext temp)
+        {
+            _context = temp;
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks(int pageSize = 5, int pageNum = 1, string sortType = "asc")
+        {
+            List<Book> books;
+            
+            if (sortType == "asc")
+            {
+                books = _context.Books
+                    .OrderBy(b => b.Title)
+                    .Skip((pageNum - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToList();
+            } else if (sortType == "desc")
+            {
+                books = _context.Books
+                    .OrderByDescending(b => b.Title)
+                    .Skip((pageNum - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToList();
+            } else
+            {
+                books = _context.Books
+                    .Skip((pageNum - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToList();
+            }
+
+            var totalNumBooks = _context.Books.Count();
+
+            var returnObject = new
+            {
+                Books = books,
+                TotalNumBooks = totalNumBooks
+            };
+            
+            return Ok(returnObject);
+        }
+    }
+}
---
+++ frontend/src/BookList.tsx
@@ -0,0 +1,134 @@
+import { useEffect, useState } from 'react';
+import { Book } from './types/Book';
+
+function BookList() {
+  const [books, setBooks] = useState<Book[]>([]);
+  const [pageSize, setPageSize] = useState<number>(5);
+  const [pageNum, setPageNum] = useState<number>(1);
+  const [totalItems, setTotalItems] = useState<number>(0);
+  const [totalPages, setTotalPages] = useState<number>(1);
+  const [sortType, setSortType] = useState<string>('');
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const response = await fetch(
+        `https://localhost:5000/Bookstore?pageSize=${pageSize}&pageNum=${pageNum}&sortType=${sortType}`
+      );
+      const data = await response.json();
+      setBooks(data.books);
+      setTotalItems(data.totalNumBooks);
+      setTotalPages(Math.ceil(totalItems / pageSize));
+    };
+
+    fetchBooks();
+  }, [pageSize, pageNum, totalItems]);
+
+  useEffect(() => {
+    const sorted = [...books].sort((a, b) =>
+      sortType === 'asc'
+        ? a.title.localeCompare(b.title)
+        : b.title.localeCompare(a.title)
+    );
+    setBooks(sorted);
+  }, [books, sortType]);
+
+  return (
+    <>
+      <h1>Book List</h1>
+      <br />
+      {books.map((b) => (
+        <div id="bookCard" className="card" key={b.bookId}>
+          <h3 className="card-title">{b.title}</h3>
+          <br />
+          <div className="card-body">
+            <ul className="list-unstyled">
+              <li>
+                <strong>Author:</strong> {b.author}
+              </li>
+              <li>
+                <strong>Publisher:</strong> {b.publisher}
+              </li>
+              <li>
+                <strong>ISBN:</strong> {b.isbn} Individuals Served
+              </li>
+              <li>
+                <strong>Classfication:</strong> {b.classification}
+              </li>
+              <li>
+                <strong>Category:</strong> {b.category}
+              </li>
+              <li>
+                <strong>Page Count:</strong> {b.pageCount}
+              </li>
+              <li>
+                <strong>Price:</strong> {b.price}
+              </li>
+            </ul>
+          </div>
+        </div>
+      ))}
+      <br />
+
+      <button disabled={pageNum === 1} onClick={() => setPageNum(pageNum - 1)}>
+        Previous
+      </button>
+
+      {[...Array(totalPages)].map((_, index) => (
+        <button
+          key={index + 1}
+          onClick={() => setPageNum(index + 1)}
+          disabled={pageNum === index + 1}
+        >
+          {index + 1}
+        </button>
+      ))}
+
+      <button
+        disabled={pageNum === totalPages}
+        onClick={() => setPageNum(pageNum + 1)}
+      >
+        Next
+      </button>
+      <br />
+
+      <label>
+        Results Per Page:
+        <select
+          value={pageSize}
+          onChange={(b) => {
+            setPageSize(Number(b.target.value));
+            setPageNum(1);
+          }}
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </label>
+      <br />
+      <br />
+
+      <label>
+        Sort Books By Title:
+        <select
+          value={sortType}
+          onChange={(t) => {
+            setSortType(t.target.value);
+          }}
+        >
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+      </label>
+    </>
+  );
+}
+
+export default BookList;
